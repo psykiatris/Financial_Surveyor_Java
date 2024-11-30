321c88e15a8f5ae1a6d0235e9b27de351718c2e6
Patrick Palczewski <psykiatris@gmail.com>
2024-11-30 07:05:03 -0800

    Re-coded implicit class for StartUp. App cannot run under terminal as it is compiled with a newer version of the JRE (Man-built) A

--- src/main/java/StartUp.java
+++ src/main/java/StartUp.java
@@ -1,21 +1,18 @@
 import account.CreditCards;
 
-import java.time.LocalDate;
+// Implicit class
+public static void main(String[] args) {
 
-public class StartUp {
-    public static void main(String[] args) {
+    CreditCards myCC = new CreditCards();
+    myCC.setPrincipal(453.22);
+    myCC.setInterestRate(4.5);
+    myCC.setStartDate(LocalDate.parse("2024-06-30"));
 
-        CreditCards myCC = new CreditCards();
-        myCC.setPrincipal(453.22);
-        myCC.setInterestRate(4.5);
-        myCC.setStartDate(LocalDate.parse("2024-06-30"));
+    // Testing - will remove later in production
+    // Retrieve info
+    myCC.printCurrentdate();
+    System.out.println("Start date: " + myCC.getStartDate());
+    System.out.println("Principal: " + myCC.getPrincipal());
+    System.out.println("Interest Rate: " + myCC.getInterestRate());
 
-        // Testing - will remove later in production
-        // Retrieve info
-        myCC.printCurrentdate();
-        System.out.println("Start date: " + myCC.getStartDate());
-        System.out.println("Principal: " + myCC.getPrincipal());
-        System.out.println("Interest Rate: " + myCC.getInterestRate());
-
-    }
 }
