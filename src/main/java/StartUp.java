07018e60b272efaba5a2de0db7b0533ef25c70f7
Patrick Palczewski <psykiatris@gmail.com>
2024-11-30 06:34:09 -0800

    Removed preview code.

--- src/main/java/StartUp.java
+++ src/main/java/StartUp.java
@@ -2,29 +2,19 @@ import account.CreditCards;
 
 import java.time.LocalDate;
 
+public class StartUp {
+    public static void main(String[] args) {
 
-// new feature - no need to explicitly state class with a main() method.
-public static void main(String[] args) {
+        CreditCards myCC = new CreditCards();
+        myCC.setPrincipal(453.22);
+        myCC.setInterestRate(4.5);
+        myCC.setStartDate(LocalDate.parse("2024-06-30"));
 
-    CreditCards myCC = new CreditCards();
-    myCC.setPrincipal(453.22);
-    myCC.setInterestRate(4.5);
-    myCC.setStartDate(LocalDate.parse("2024-06-30"));
+        // Retrieve info
+        myCC.printCurrentdate();
+        System.out.println("Start date: " + myCC.getStartDate());
+        System.out.println("Principal: " + myCC.getPrincipal());
+        System.out.println("Interest Rate: " + myCC.getInterestRate());
 
-    // Retrieve info
-    myCC.printCurrentdate();
-    System.out.println("Start date: " + myCC.getStartDate());
-    System.out.println("Principal: " + myCC.getPrincipal());
-    System.out.println("Interest Rate: " + myCC.getInterestRate());
-
-
-    // Printing currency
-    // Getting currency code
-
-
-
-
-
+    }
 }
-
-
