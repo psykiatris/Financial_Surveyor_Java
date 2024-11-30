ad4dbfaacc1b21c3913c3194f87d2dbf77aa12fb
Patrick Palczewski <psykiatris@gmail.com>
2024-11-30 07:36:33 -0800

    Attempting to not use any preview features. This compiles under JRE 23

--- src/main/java/StartUp.java
+++ src/main/java/StartUp.java
@@ -1,21 +1,28 @@
 import account.CreditCards;
 
-// Implicit class
-public static void main(String[] args) {
-    StartUp.args = args;
+import java.time.LocalDate;
 
-    CreditCards myCC = new CreditCards();
-    myCC.setPrincipal(453.22);
-    myCC.setInterestRate(4.5);
-    myCC.setStartDate(LocalDate.parse("2024-06-30"));
+/* This will be the main start-up for the application.
+All menus and selections will be entered here,
+then the appropriate class & method will be called.
+ */
 
-    // Testing - will remove later in production
-    // Retrieve info
-    myCC.printCurrentdate();
-    System.out.println("Start date: " + myCC.getStartDate());
-    System.out.println("Principal: " + myCC.getPrincipal());
-    System.out.println("Interest Rate: " + myCC.getInterestRate());
+public class StartUp {
+    public static void main(String[] args) {
+
+        CreditCards myCC = new CreditCards();
+        myCC.setPrincipal(453.22);
+        myCC.setInterestRate(4.5);
+        myCC.setStartDate(LocalDate.parse("2024-06-30"));
+
+        // Testing - will remove later in production
+        // Retrieve info
+        myCC.printCurrentdate();
+        System.out.println("Start date: " + myCC.getStartDate());
+        System.out.println("Principal: " + myCC.getPrincipal());
+        System.out.println("Interest Rate: " + myCC.getInterestRate());
+
+    }
 
 }
 
-        private static String[] args;
