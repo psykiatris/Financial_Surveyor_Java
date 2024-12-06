e5dcde3481875ee5a7dae12c960867635a772168
Patrick Palczewski <psykiatris@gmail.com>
2024-12-06 07:06:03 -0800

    Re-writing code. Deleted current class files

--- src/main/java/StartUp.java
+++ src/main/java/StartUp.java
@@ -1,28 +1,26 @@
-import account.CreditCards;
-
-import java.time.LocalDate;
-
 /* This will be the main start-up for the application.
 All menus and selections will be entered here,
 then the appropriate class & method will be called.
  */
 
+import account.Account;
+
+import java.time.LocalDate;
+
 public class StartUp {
     public static void main(String[] args) {
 
-        CreditCards myCC = new CreditCards();
-        myCC.setPrincipal(453.22);
-        myCC.setInterestRate(4.5);
-        myCC.setStartDate(LocalDate.parse("2024-06-30"));
 
-        // Testing - will remove later in production
-        // Retrieve info
-        myCC.printCurrentdate();
-        System.out.println("Start date: " + myCC.getStartDate());
-        System.out.println("Principal: " + myCC.getPrincipal());
-        System.out.println("Interest Rate: " + myCC.getInterestRate());
 
-    }
+       cc.setStartDate(LocalDate.parse("2025-01-01"));
+
+
+
+       System.out.println("Start date: " + cc.getStartDate());
+       System.out.println("Interest rate: " + cc.getInterestRate());
+       System.out.println("Principal: " + cc.getPrincipal());
+
+
 
 }
-
+}
