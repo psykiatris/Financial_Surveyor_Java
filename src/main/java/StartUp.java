981a82cd0c02fef2c3f5655ec1c8ecb6840d67eb
Patrick Palczewski <psykiatris@gmail.com>
2024-12-12 05:47:29 -0800

    Created abstract BaseAccount (supertype) and Account (generic type).

--- src/main/java/StartUp.java
+++ src/main/java/StartUp.java
@@ -3,24 +3,10 @@ All menus and selections will be entered here,
 then the appropriate class & method will be called.
  */
 
-import account.Account;
-
 import java.time.LocalDate;
 
 public class StartUp {
     public static void main(String[] args) {
 
-
-
-       cc.setStartDate(LocalDate.parse("2025-01-01"));
-
-
-
-       System.out.println("Start date: " + cc.getStartDate());
-       System.out.println("Interest rate: " + cc.getInterestRate());
-       System.out.println("Principal: " + cc.getPrincipal());
-
-
-
-}
+    }
 }
---
+++ src/main/java/account/BaseAccount.java
@@ -0,0 +1,38 @@
+package account;
+/*
+This is an abstract of account, with data and methods. Subclasses
+will derive from this class, and work with the generic
+Account class for account management.
+ */
+
+import java.time.LocalDate;
+
+public abstract class BaseAccount {
+
+    protected double principal;
+    protected double interestRate;
+    protected LocalDate creationDate;
+    protected LocalDate startDate;
+    protected int term;
+
+    public BaseAccount(double p, double i, LocalDate sDate, int t) {
+        this.principal = p;
+        this.interestRate = i;
+        this.startDate = sDate;
+        this.term = t;
+        this.creationDate = LocalDate.now();
+    }
+
+    public BaseAccount() {
+        this.creationDate = LocalDate.now();
+    }
+
+    // Methods to handle different scenarios
+    public abstract double getInterestRate();   // APR on loan or savings
+    public abstract double getInterest();   // Colllect total interest calculated for term
+    public abstract double getBalance();    // Principal balance
+    public abstract void setInterestRate(double i);
+    public abstract void setStartDate(LocalDate beginDate);
+    public abstract void setTerm(int t);
+
+}
